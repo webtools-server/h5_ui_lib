e40d07c8965f57d13df29b355a33670e51545b15
xi <xiaohai.xi@jyblife.com>
2018-06-06 11:46:12 +0800

    Publish
     - @jyb/ui-vue-switch@0.1.1

---
+++ lib/packages/vue-switch/src/switch.vue
@@ -0,0 +1,27 @@
+<template>
+  <span class="ui-switch-btn" :class="classes" @click="onClick"></span>
+</template>
+
+<script>
+export default {
+  name: 'ui-switch',
+  props: {
+    on: {
+      type: [Number, Boolean, String],
+      default: 0,
+    }
+  },
+  computed: {
+    classes() {
+      return {
+        'ui-switch-btn--close': this.on != 1
+      }
+    }
+  },
+  methods: {
+    onClick() {     
+      this.$emit('update:on', this.on == 0 ? 1 : 0);
+    }
+  }
+}
+</script>
