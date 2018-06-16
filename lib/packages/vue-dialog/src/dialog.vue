d2c1f797005fbead0644e9e44ab8286e2ef3763d
xi <xiaohai.xi@jyblife.com>
2018-06-16 16:34:03 +0800

    build:dialog add class options

--- lib/packages/vue-dialog/src/dialog.vue
+++ lib/packages/vue-dialog/src/dialog.vue
@@ -1,13 +1,13 @@
 <template>
   <div ref="popup" v-if="open" class="ui-dialog">
-    <div v-if="title" class="ui-dialog__title">
+    <div v-if="title" class="ui-dialog__title" :class="titleClass">
       {{ title }}
       <span class="ui-dialog__fn-wrap" v-if="showClose" @click="close"><i class="ui-icon-close"></i></span>
     </div>
-    <div class="ui-dialog__content">
+    <div class="ui-dialog__content" :class="contentClass">
       <slot name="content"></slot>
     </div>
-    <div v-if="$slots.buttons" class="ui-dialog__footer">
+    <div v-if="$slots.buttons" class="ui-dialog__footer" :class="footerClass">
       <slot name="buttons"></slot>
     </div>
   </div>
@@ -25,6 +25,18 @@ export default {
     showClose: {
       type: Boolean,
       default: false,
+    },
+    titleClass: {
+      type: Array,
+      default: () => [],
+    },
+    contentClass: {
+      type: Array,
+      default: () => [],
+    },
+    footerClass: {
+      type: Array,
+      default: () => [],   
     }
   },
   methods: {
