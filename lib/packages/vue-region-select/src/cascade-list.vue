604c5b8ba78c5a77a9b9a45eac175972b9f24e0a
xi <xiaohai.xi@jyblife.com>
2019-04-15 15:12:37 +0800

    #0000 feat:添加地区选择vue组件

--- lib/packages/vue-region-select/src/cascade-list.vue
+++ lib/packages/vue-region-select/src/cascade-list.vue
@@ -1,42 +1,160 @@
 <template>
-  <div  v-if="!item.children" class="ui-region-select__list">
-    <ui-cascade-item v-for="item in data"
-      :active=""
-      :disabled=""
-      @click="change(item, depth)"
-    >{{ item.text }}</ui-cascade-item>    
-  </div>
-  <ui-cascade-list v-else 
-    :data="item.children"
-    :depth="depth + 1"
-  ></ui-cascade-list>
+<div>
+    <div class="pcr-wrap">
+        <div v-for="list, idx in lists"
+            v-show="list && list.length !== 0"
+            :class="{chosen: true, cur: currentShow === idx}"
+            :style="{'max-width': pcrWidth}"            
+            @click="togglePrcType(idx)">    
+            <span class="text">{{ currentSelect[idx] && currentSelect[idx].name || (currentShow === idx ? '请选择' : '')}}</span>
+        </div>
+    </div>
+    <div class="region-wrap"
+        ref="regionWrap"
+        :style="{width:wrapWidth}"        
+        @transitionend="onTransitionEnd">
+        <ul class="pcr-list"                    
+            v-for="list, idx in lists"
+            :style="{width: innerWidth + 'px', height: getListHeight(idx)}">
+            <li v-for="item, index in list"
+                :key="item.id"
+                :class="{ chosen : isCurrent(idx, item.id) }"
+                @click="change(idx, item)">{{ item.name }}
+                <i v-if="isCurrent(idx, item.id)" class="icon checked-icon"></i>
+            </li>
+        </ul>       
+    </div>
+</div>
 </template>
 
 <script>
-import CascadeItem from './cascade-item.vue';
+import { preventDocMove, enableDocMove, getIOSVer } from "./util";
+const iosVer = getIOSVer()[0];
 
 export default {
-  name: 'ui-cascade-list',
+  name: "ui-cascade-list",
   props: {
-    data: {
+    lists: {
       type: Array,
+      required: true
     },
-    tag: {
-      type: String,
-      default: 'ul'
-    },
-    depth: {
+    maxLevel: {
       type: Number,
-      default: 0,
+      required: true,
+      default: 4
+    },
+    preChosen: {
+      type: Array
     }
   },
-  components: {
-    [CascadeItem.name]: CascadeItem,
+  data() {
+    return {
+      currentSelect: (this.preChosen && this.preChosen.slice()) || [],
+      currentShow: 0,
+      innerWidth: 0,
+      animating: false
+    };
+  },
+  computed: {
+    wrapWidth: function() {
+      return this.innerWidth * this.lists.length + "px";
+    },
+    itemWidth: function() {
+      return this.innerWidth + "px";
+    },
+    pcrWidth: function() {
+      return 100 / this.maxLevel + "%";
+    },
+    shift: function() {
+      return -this.currentShow * this.innerWidth;
+    }
+  },
+  watch: {
+    lists(val) {
+      const newList = val[this.currentShow + 1];
+      if (newList) {
+        if (newList.length === 0) {
+          this.$emit("chosen-complete", this.currentSelect);
+        } else {
+          this.currentShow += 1;
+          this.reset();
+        }
+      } else {
+        return;
+      }
+    },
+    currentShow(val) {
+      this.doAnim();
+    }
   },
   methods: {
-    change(item, depth) {
+    reset() {
+      for (
+        let i = this.currentShow, len = this.currentSelect.length;
+        i < len;
+        i++
+      ) {
+        this.currentSelect.splice(i, 1, null);
+      }
+    },
+    togglePrcType(idx) {
+      if (idx > 0 && !this.currentSelect[idx - 1]) {
+        return;
+      }
 
-    }
+      this.currentShow = idx;
+    },
+    isCurrent(idx, id) {
+      if (!this.currentSelect[idx]) {
+        return false;
+      }
+
+      return this.currentSelect[idx].id === id;
+    },
+    change(idx, item) {
+      if (this.animating) {
+        return;
+      }
+
+      this.currentSelect.splice(idx, 1, item);
+      if (idx === this.maxLevel - 1) {
+        this.$emit("chosen-complete", this.currentSelect);
+      } else {
+        this.$emit("nextLevel", idx, item.id);
+      }
+    },
+    doAnim() {
+      const el = this.$refs.regionWrap;
+      if (el) {
+        setTimeout(() => {
+          el.scrollTop = 0;
+        });
+
+        const list = el.children[this.currentShow];
+        if (list) list.scrollTop = 0;
+
+        this.animating = true;
+        el.style.webkitTransform = `translateX(${this.shift}px)`;
+      }
+    },
+    getListHeight(idx) {
+      if (this.currentShow == idx) {
+        if (iosVer == 8) {
+          return "6.5rem";
+        }
+
+        return "auto";
+      }
+
+      return "0px";
+    },
+    onTransitionEnd() {
+      this.animating = false;
+    },
+    doNothing() {}
+  },
+  created() {
+    this.innerWidth = window.innerWidth;
   }
-}
+};
 </script>
--- lib/packages/vue-region-select/src/region-select.vue
+++ lib/packages/vue-region-select/src/region-select.vue
@@ -1,16 +1,54 @@
 <template>
-  <div class="ui-region-select">
-    <div class=""></div>
-    <div class=""></div>
-  </div>
+<popup :open="show"
+  :title-border=false
+  :re-caculate-content-height="false"
+  @update:open="onUpdateOpen">
+  <div slot='title'>选择所在地区</div>
+  <ui-cascade-list slot='content'        
+    :lists="lists"
+    :maxLevel=4
+    :preChosen="chosenRegion"
+    @nextLevel="getLevelData"
+    @chosen-complete="onChosenComplete">
+  </ui-cascade-list>
+</popup>
 </template>
 
 <script>
-import CascadeList from './cascade-list.vue';
+import Popup from "@jyb/ui-vue-popup";
+import CascadeList from './cascade-list.vue'
 export default {
   name: 'ui-region-select',
   components: {
-    [CascadeList.name]:  CascadeList,
+    [Popup.name]: Popup,
+    [CascadeList.name]: CascadeList,
+  },
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+    lists: {
+      type: Array,
+      required: true
+    },
+    chosenRegion: {
+      type: Array,
+      default: [],
+    }
+  },
+  methods: {
+    getLevelData(level, id) {
+      this.$emit('nextLevel', level, id)
+    },
+    onChosenComplete (chosen) {
+      this.$emit('chosen-complete', chosen)
+    },
+    onUpdateOpen(val) {
+      if (!val) {
+        this.$emit('close', 'close');
+      }
+    }
   }
 }
 </script>
